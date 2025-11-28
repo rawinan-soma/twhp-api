0352e38084154fa69c956b29c29574af15c513a0
rawinan.s <rawinan.soma@gmail.com>
2025-11-28 08:42:18 +0700

    before init prisma 7

--- src/admins/admins.module.ts
+++ src/admins/admins.module.ts
@@ -3,9 +3,10 @@ import { AdminsService } from './admins.service';
 import { PrismaService } from 'prisma/prisma.service';
 import { AdminsController } from './admins.controller';
 import { FactoriesService } from 'src/factories/factories.service';
+import { EnrollsService } from 'src/enrolls/enrolls.service';
 
 @Module({
-  providers: [AdminsService, PrismaService, FactoriesService],
+  providers: [AdminsService, PrismaService, FactoriesService, EnrollsService],
   controllers: [AdminsController],
 })
 export class AdminsModule {}
